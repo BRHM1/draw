05536af3f8338895881434bf0355817679f166c9
BRHM1 <brhm@std.mans.edu.eg>
2024-09-20 11:42:17 +0300

    made a component for rendering canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,50 +1,45 @@
 import React, {
-  useRef,
   useLayoutEffect,
+  useRef,
   useState,
 } from "react";
-import { twMerge } from "tailwind-merge";
 import rough from "roughjs/bundled/rough.esm";
+import { twMerge } from "tailwind-merge";
 import { useStore } from "../store";
 
-import { Rectangle, Circle, Ellipse, Line, Path } from "../hooks/elementModule";
+import { Circle, Ellipse, Line, Path, Rectangle } from "../hooks/elementModule";
 
-import useDraw from "../hooks/useDraw";
-import useErase from "../hooks/useErase";
-import useShape from "../hooks/useShape";
-import useSelect from "../hooks/useSelect";
-import useText from "../hooks/useText";
-import Toolbar from "./Toolbar";
 import Button from "./Button";
 import OptionsToolbar from "./OptionsToolbar";
 import PenOptionsToolbar from "./PenOptionsToolbar";
+import RenderingCanvas from "./RenderingCanvas";
+import Toolbar from "./Toolbar";
 
-import { drawElement } from "../utils/utils";
 
 const Canvas = () => {
-  const elements = useStore((state) => state.elements);
-  const options = useStore((state) => state.options);
-  const penOptions = useStore((state) => state.penOptions);
-  const shapes = new Set(["rectangle", "ellipse", "line", "circle"]);
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const textRef = useRef(null);
   const shapeRef = useRef(null);
   const [cordinates, setCordinates] = useState({ x: 0, y: 0 });
-  const generator = rough.generator();
-  const addElement = useStore((state) => state.addElement);
-  const removeLastElement = useStore((state) => state.removeLastElement);
-  const renderingCanvasRef = useRef(null);
-  const renderedElements = useRef([]);
   const [isDrawing, setIsDrawing] = useState(false);
+
+  const options = useStore((state) => state.options);
+  const penOptions = useStore((state) => state.penOptions);
+  const addElement = useStore((state) => state.addElement);
+  const elements = useStore((state) => state.elements);
+  const action = useStore((state) => state.action);
+  const type = useStore((state) => state.type);
+  
+  
+  const generator = rough.generator();
+  const shapes = new Set(["rectangle", "ellipse", "line", "circle"]);
+
   // const fn = (e) => {
   //   setCordinates({ x: e.clientX, y: e.clientY });
   // };
   // window.addEventListener("mousemove", fn);
 
-  // this is the logic behind the toolbar connection with the canvas 17 - 46
-  const action = useStore((state) => state.action);
-  const type = useStore((state) => state.type);
 
   const reFocus = () => {
     if (textRef.current !== null) textRef.current.value = "";
@@ -52,17 +47,6 @@ const Canvas = () => {
       textRef?.current?.focus();
     }, 0);
   };
-  const renderingContextRef = useRef(null);
-
-  // ISSUE: Hooks are called each time this component re-renders and this affects the performance
-  // CRITICAL TODO: change the way those functions invoked ---------------------------------------------------------------
-  // const actionTypes = {
-  //   draw: useDraw(),
-  //   erase: useErase(),
-  //   shape: useShape(),
-  //   select: useSelect(contextRef),
-  //   text: useText(reFocus, canvasRef),
-  // };
 
   let Down = (e) => {
       // based on the type an object of that type will be created
@@ -128,32 +112,19 @@ const Canvas = () => {
             0
           );
       }
-      addElement(shapeRef.current);
-      // const [down, _, __] = Object.values(actionTypes[action]);
-      // down(e);
     },
     Move = (e) => {
-      // const [_, move, __] = Object.values(actionTypes[action]);
       if (e.buttons !== 1) return;
       shapeRef.current.updateDimensions(e.clientX, e.clientY, generator);
       setIsDrawing(!isDrawing);
-      // addElement(shapeRef.current);
-      // removeLastElement();
-
-      // move(e);
     },
     Up = (e) => {
-      // const [_, __, up] = Object.values(actionTypes[action]);
-      // up(e);
-      renderedElements.current.push(shapeRef.current);
+      contextRef.current.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      addElement(shapeRef.current);
     };
-  // CRITICAL TODO: change the way those functions invoked ---------------------------------------------------------------
-    // TRAP: the rendering effect is called each time the component re-renders even there is no change in the elements
- 
 
-  // drawing effect
+
   useLayoutEffect(() => {
-    console.log("drawing rendered")
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
@@ -172,48 +143,15 @@ const Canvas = () => {
     canvas.style.height = `${rect.height}px`;
   
 
-    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
     const roughCanvas = rough.canvas(canvas);
-  //   const lastElement = elements[elements.length - 1];
-  //  if(lastElement) shapes.has(lastElement.type)
-  //     ? lastElement.draw(roughCanvas)
-  //     : lastElement.draw(context);
+    
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
     shapeRef.current && shapes.has(shapeRef.current.type) && shapeRef.current.draw(roughCanvas);
     shapeRef.current && !shapes.has(shapeRef.current.type) && shapeRef.current.draw(context);
-    // Draw the last element on the context and draw the elemenets on the rendering canvas
 
-
-    console.log("elements are:", elements);
     contextRef.current = context;
-    // return () => window.removeEventListener("mousemove", fn);
   }, [isDrawing]);
 
-
-  useLayoutEffect(() => {
-    console.log("rendered rendered rendered rendered rendered rendered")
-    const renderingCanvas = renderingCanvasRef.current;
-    const renderingCanvasContext = renderingCanvas.getContext("2d");
-
-    const dpr = window.devicePixelRatio;
-    const rect = renderingCanvas.getBoundingClientRect();
-
-    renderingCanvas.width = rect.width * dpr;
-    renderingCanvas.height = rect.height * dpr;
-    
-    renderingCanvasContext.scale(dpr, dpr);
-
-    const roughCanvas = rough.canvas(renderingCanvas);
-    renderingCanvas.style.height = `${rect.height}px`;
-    renderingCanvas.style.width = `${rect.width}px`;
-    // renderingCanvas.style.backgroundColor = `transparent`;
-    console.log(renderedElements.current)
-    renderedElements.current.forEach((element) => {
-      shapes.has(element.type)
-        ? element.draw(roughCanvas)
-        : element.draw(renderingContextRef.current);
-    });
-    renderingContextRef.current = renderingCanvasContext;
-  }, [renderedElements.current.length]);
 
   return (
     <div className="w-full h-screen grid">
@@ -273,7 +211,7 @@ const Canvas = () => {
         // onMouseLeave={Up}
         // onMouseEnter={Move}
       />
-      <canvas ref={renderingCanvasRef} className="row-start-1 col-start-1 min-w-full min-h-full overflow-hidden"></canvas>
+      <RenderingCanvas />      
       {action === "shape" && <OptionsToolbar />}
       {action === "draw" && <PenOptionsToolbar />}
       <div className="w-40 flex justify-center items-center gap-2 mb-2 ml-2">
---
+++ src/components/RenderingCanvas.jsx
@@ -0,0 +1,44 @@
+import { useRef, useLayoutEffect } from "react";
+import rough from "roughjs/bundled/rough.esm";
+import { useStore } from "../store";
+
+const RenderingCanvas = () => {
+  const renderingCanvasRef = useRef(null);
+  const renderingContextRef = useRef(null);
+  const shapes = new Set(["rectangle", "ellipse", "line", "circle"]);
+  const elements = useStore((state) => state.elements);
+
+  useLayoutEffect(() => {
+    const renderingCanvas = renderingCanvasRef.current;
+    const renderingCanvasContext = renderingCanvas.getContext("2d");
+    const dpr = window.devicePixelRatio;
+    const rect = renderingCanvas.getBoundingClientRect();
+    
+    renderingCanvas.width = rect.width * dpr;
+    renderingCanvas.height = rect.height * dpr;
+    
+    renderingCanvasContext.scale(dpr, dpr);
+    
+    renderingCanvas.style.height = `${rect.height}px`;
+    renderingCanvas.style.width = `${rect.width}px`;
+    
+    const roughCanvas = rough.canvas(renderingCanvas);
+    
+    renderingCanvasContext.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    elements.forEach((element) => {
+      shapes.has(element.type)
+        ? element.draw(roughCanvas)
+        : element.draw(renderingContextRef.current);
+    });
+    renderingContextRef.current = renderingCanvasContext;
+  }, [elements]);
+
+  return (
+    <canvas
+      ref={renderingCanvasRef}
+      className="row-start-1 col-start-1 min-w-full min-h-full overflow-hidden"
+    ></canvas>
+  );
+};
+
+export default RenderingCanvas;
